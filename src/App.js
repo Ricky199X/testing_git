1163d1e37aee8f8309d5e826078445257619ca7c
Ricky199X <rickyrojas19@gmail.com>
2020-06-05 19:03:25 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NoMatch from "./components/NoMatch";
 import HomePage from "./containers/HomePage";
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <>
+
       <Router>
+        <NavBar />
         <div className="App">
           <h1>This is the App.js file - we are testing Github branches.</h1>
           <body>I am adding a body here to practice documenting a change.</body>

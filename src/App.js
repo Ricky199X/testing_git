8509529c8c0e235217cd9f56f056340508186951
Michael Chen <michaelchengaming@gmail.com>
2020-06-05 13:19:54 -0700

    add react router dom and inplement ghetto 404 page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is the App.js file - we are testing Github branches.</h1>
-      <body>
-        I am adding a body here to practice documenting a change.
-      </body>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <div className="App">
+            <h1>This is the App.js file - we are testing Github branches.</h1>
+            <body>I am adding a body here to practice documenting a change.</body>
+            {/* <Route path="/" exact component={ChickenButt} /> */}
+            <Route component={NoMatch} />
+          </div>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
